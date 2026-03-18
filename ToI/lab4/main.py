7c62a3ee4aa7fccb8c92cc84a00fc7cbe098f61e
Ilya <shefer-2006@mail.ru>
2026-03-18 15:22:19 +0300

    lab4 complete

--- ToI/lab4/main.py
+++ ToI/lab4/main.py
@@ -14,22 +14,40 @@ for l in range(0, 3):
     if comb in d:
       d[comb][0] += 1
     else:
-      d[comb] = [1, "", 0, 0, 0] # ** N, code, p, q, len 
+      d[comb] = [1, 0, 0, 0, 0, ""] # ** N, p, I, q, l, c
     N += 1
-  dArrs.append([d, N])
+    
+  dArrs.append([d, N, l, 0])
 
-for d, l in dArrs:
+for d, l, it, _ in dArrs:
   sortedDict = dict(sorted(d.items(), key=lambda x: x[1], reverse=True))
   q = 0
-  for elem in sortedDict:
-    p = sortedDict[elem][0] / l
-    sortedDict[elem][2] = p
-    sortedDict[elem][3] = q
-    sortedDict[elem][4] = math.ceil(-math.log2(p))
-    q += p
-
-
-
-for d, _ in dArrs:
+  midLen = 0
+  for key in sortedDict:
+    p = sortedDict[key][0] / l
+    I = -math.log2(p)
+    lenght = math.ceil(I)
+    sortedDict[key][1] = p
+    sortedDict[key][2] = I
+    sortedDict[key][3] = q
+    sortedDict[key][4] = lenght
+    qc = q
+    s = ""
+    for i in range(lenght):
+      p2 = math.pow(2, -1 * (i + 1))
+      if(qc >= p2):
+        s += '1'
+        qc -= p2
+      else:
+        s += '0'
+    sortedDict[key][5] = s
+    q += pl = 7.14620
+R = 3.57310
+    midLen += p * lenght
+  dArrs[it][3] = midLen
+  dArrs[it][0] = sortedDict
+    
+for d, _, it, l in dArrs:
   for key in d:
-    print(key, d[key])
+    print(f'|{key:^5}|{d[key][0]:^4}| {d[key][1]:.10f} | {d[key][2]:.5f} | {d[key][3]:.5f} |{d[key][4]:^3}|{d[key][5]:^10}|')
+  print(f'\nl = {l:.5f}\nR = {l/(it + 1):.5f}\n\n')
